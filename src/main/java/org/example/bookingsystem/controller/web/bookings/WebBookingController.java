1f4fe6ea47fea763746781b37449a27bcb7713ff
Yuliya <Yulechka140200@gmail.com>
2026-04-30 10:45:12 +0500

    доработан фильтр записей, подредактирован интерфейс в изменении записей

--- src/main/java/org/example/bookingsystem/controller/web/bookings/WebBookingController.java
+++ src/main/java/org/example/bookingsystem/controller/web/bookings/WebBookingController.java
@@ -5,19 +5,19 @@ import org.example.bookingsystem.model.Role;
 import org.example.bookingsystem.model.User;
 import org.example.bookingsystem.service.BookingService;
 import org.example.bookingsystem.service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
+import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 
 @Controller
 @RequestMapping("/web/bookings")
